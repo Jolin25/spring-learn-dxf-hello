ae00f6834da7124f778b897f6a8cdecd844f4d0b
jrl <584189010@qq.com>
2021-01-11 17:24:42 +0800

    init hello spring

---
+++ src/main/java/top/spring/learn/firstspring/FirstSpringApplication.java
@@ -0,0 +1,23 @@
+package top.spring.learn.firstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 小虫子的小日常
+ */
+@SpringBootApplication
+@RestController
+public class FirstSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstSpringApplication.class, args);
+	}
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello spring";
+	}
+}
